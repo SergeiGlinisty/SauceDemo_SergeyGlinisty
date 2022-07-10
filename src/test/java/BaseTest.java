87bd2a69823e7d9e47202068fba3bbf5aea95fa3
SergeiGlinisty <sergei.glinisty@gmail.com>
2022-07-10 15:26:10 +0300

    learning locators and testing the site's shopping cart https://www.saucedemo.com/

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,40 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations .*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class BaseTest {
+    protected WebDriver driver;
+
+    @BeforeClass
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--start-maximized");
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    }
+
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
+
+    }
+}
---
+++ src/test/java/Locators.java
@@ -0,0 +1,77 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class Locators extends BaseTest {
+
+                                                     //xpath
+//Поиск по атрибуту, например By.xpath("//tag[@attribute='value']");
+    //https://www.saucedemo.com/
+    WebElement attribute = driver.findElement(By.xpath("//div[@class='login_logo']"));
+
+//Поиск по тексту, например By.xpath("//tag[text()='text']");
+    //https://www.saucedemo.com/inventory.html
+    WebElement text = driver.findElement(By.xpath("//button[text()='Open Menu']"));
+
+//Поиск по частичному совпадению атрибута, например By.xpath("//tag[contains(@attribute,'text')]");
+    //https://www.saucedemo.com/
+    WebElement containsAttribute = driver.findElement(By.xpath("//div[contains(@class,'bot')]"));
+
+//Поиск по частичному совпадению текста, например By.xpath("//tag[contains(text(),'text')]");
+    //https://www.saucedemo.com/
+    WebElement containsText = driver.findElement(By.xpath("//noscript[contains(text(),'to enable')]"));
+
+//ancestor, например //*[text()='Enterprise Testing']//ancestor::div
+    //https://www.saucedemo.com/inventory.html
+    WebElement ancestor = driver.findElement(By.xpath("//*[text()='Add to cart']//ancestor::body"));
+
+//descendant
+    //https://www.saucedemo.com/inventory-item.html?id=4
+    WebElement descendant = driver.findElement(By.xpath("//div[@class='header_secondary_container']//descendant::button"));
+
+//following-sibling
+    //https://www.saucedemo.com/cart.html
+    WebElement following_sibling = driver.findElement(By.xpath("//a[@id ='item_5_title_link']//following-sibling::div"));
+
+//parent
+    //https://www.saucedemo.com/cart.html
+    WebElement parent = driver.findElement(By.xpath("//span[contains(text(), 'Your')]//parent::*"));
+
+//preceding - sibling
+    //https://www.saucedemo.com/checkout-step-one.html
+    WebElement preceding_sibling = driver.findElement(By.xpath("//div[@id= 'checkout_info_container']//preceding-sibling::*"));
+
+//*поиск элемента с условием AND, например //input[@class='_2zrpKA _1dBPDZ' and @type='text']
+    //https://www.saucedemo.com/checkout-step-one.html
+    WebElement two_conditions = driver.findElement(By.xpath("//div[@id='header_container' and contains(@class, 'header')]"));
+
+                                                 //css
+//.class
+    WebElement element = driver.findElement(By.cssSelector(".login_wrapper"));
+//.class1.class2
+    WebElement elements = driver.findElement(By.cssSelector(".page_wrapper.contents_wrapper"));
+//.class1 .class2
+    WebElement overview= driver.findElement(By.cssSelector(".summary_info .summary_info_label"));
+//#id
+    WebElement id= driver.findElement(By.cssSelector("#root"));
+//tagName
+    List<WebElement> tagName= driver.findElements(By.cssSelector("div"));
+//tagName.class
+    WebElement tagNameClass= driver.findElement(By.cssSelector("div.page_wrapper"));
+//[attribute=value]
+    WebElement attributeValue= driver.findElement(By.cssSelector("[style='height: 100%;']"));
+//[attribute~=value]
+    WebElement buttonRemoveShirt= driver.findElement(By.cssSelector("[class~='btn']"));
+//[attribute|=value]
+    WebElement buttonRemove= driver.findElement(By.cssSelector("[name|='remove-sauce']"));
+//[attribute^=value]
+    WebElement linkStart= driver.findElement(By.cssSelector("a[href^='#']"));
+//[attribute$=value]
+    WebElement linkEnd= driver.findElement(By.cssSelector("div[id$='ner']"));
+//[attribute*=value]
+    WebElement linkContains= driver.findElement(By.cssSelector("div[class*='or']"));
+    }
+
+
+
